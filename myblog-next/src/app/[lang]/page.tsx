f8c9332f3de781cc2ba12fdf4bd5a761d3ed138e
김준겸 <wnsruadl90@naver.com>
2025-08-19 17:52:14 +0900

    feat : 번역함수의 Next.js 13 App Router: params Promise와 다국어(t) 사용

--- myblog-next/src/app/[lang]/page.tsx
+++ myblog-next/src/app/[lang]/page.tsx
@@ -1,22 +1,24 @@
 "use client";
 
+import React from "react";
 import { useEffect } from "react";
 import { useTranslation } from "react-i18next";
-interface Props {
-  params: { lang: string };
-}
 
-export default function Page({ params }: Props) {
+
+export default function Page({ params }: { params: Promise<{ lang: string }> }) {
   const { t, i18n } = useTranslation();
-
+  const resolvedParams = React.use(params);
+  const lang = resolvedParams.lang;
+  
   // URL param 기반 언어 전환
   useEffect(() => {
-    i18n.changeLanguage(params.lang);
-  }, [params.lang, i18n]);
+    i18n.changeLanguage(lang);
+  }, [lang, i18n]);
 
   return (
     <div>
-      <h1>{params.lang}</h1>
+      <h1>{lang}</h1>
+      <h1>{lang}</h1>
       <h1>{t("title")}</h1>
       <p>{t("description")}</p>
     </div>
