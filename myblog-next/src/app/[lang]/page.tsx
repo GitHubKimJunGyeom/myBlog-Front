b1e83a920c2ce32f28687ffe8b9da99a56a223e2
김준겸 <wnsruadl90@naver.com>
2025-08-20 11:50:40 +0900

    chore : 하나의 임포트로 사용.

--- myblog-next/src/app/[lang]/page.tsx
+++ myblog-next/src/app/[lang]/page.tsx
@@ -1,9 +1,7 @@
 "use client";
 
-import React from "react";
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import { useTranslation } from "react-i18next";
-
 
 export default function Page({ params }: { params: Promise<{ lang: string }> }) {
   const { t, i18n } = useTranslation();
