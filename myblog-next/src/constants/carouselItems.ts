27c3ed63cce6719b57940c957210a8c9367cd1aa
김준겸 <wnsruadl90@naver.com>
2025-08-21 11:12:39 +0900

    feat : 캐러셀 컨스탄트 추가

---
+++ myblog-next/src/constants/carouselItems.ts
@@ -0,0 +1,42 @@
+
+// constants/carouselItems.ts
+import type { CarouselItem } from '@/types/carousel';
+
+// Define the structure of a carousel item
+export const carouselItems: CarouselItem[] = [
+  {
+    to: '/database/programming_insight_list',
+    text: 'programming_insight_list',
+    img: '/static/assets/img/png/ProgrammingInsightListAguimanChan.png'
+  },
+  {
+    to: '/database/business_insight_list',
+    text: 'business_insight_list',
+    img: '/static/assets/img/png/BusinessInsightListAguimanChan.png'
+  },
+  {
+    to: '/database/technical_term_list',
+    text: 'technical_term_list',
+    img: '/static/assets/img/png/TechnicalTermAguimanChan.png'
+  },
+  {
+    to: '/database/clipping_list',
+    text: 'clipping_list',
+    img: '/static/assets/img/png/ClippingListAguimanChan.png'
+  },
+  {
+    to: '/introduce',
+    text: 'introduce',
+    img: '/static/assets/img/png/IntroduceAguimanChan.png'
+  },
+  {
+    to: '/contact',
+    text: 'contact.title',
+    img: '/static/assets/img/png/ContactAguimanChan.png'
+  },
+  {
+    to: '/pathnote',
+    text: 'pathnote',
+    img: '/static/assets/img/png/PathNoteAguimanChan.png'
+  }
+];
